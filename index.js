2e4fad9cba92b28237826fb9758aaf9a6ee99391
ishrafil2233 <ishrafil2233@gmail.com>
2023-02-21 22:49:19 +0600

    add mongodb and create get and post method

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+// middle ware 
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.d1cviry.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    try {
+        const database = client.db('car-service');
+        const serviceCollection = database.collection('services');
+        const orderCollection = database.collection('orders');
+
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const service = serviceCollection.find(query);
+            const result = await service.toArray();
+            res.send(result);
+        });
+
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await serviceCollection.findOne(query);
+            res.send(result);
+        });
+
+        app.get('/orders', async (req, res) => {
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const order = orderCollection.find(query);
+            const result = await order.toArray()
+            res.send(result)
+        })
+
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result)
+        })
+    }
+    finally { }
+}
+run().catch(err => console.log(err))
+
+
+app.get('/', (req, res) => {
+    res.send('Ishrafil car service is running...')
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`)
+})
